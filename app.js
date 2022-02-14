7590cd321fb46c0a3b179804c06e0180449ad1ee
Raymond Lam <raymond.m.lam@gmail.com>
2022-02-14 16:45:15 +0100

    Created functions for generating parameters

--- app.js
+++ app.js
@@ -1,15 +1,47 @@
 /**
- * Functions for generating parameters
+ * DOM elements
+ */
+
+/**
+ * Functions for generating parameters using ASCII
  */
 
 // Generates lowercase
-const getLower = () => {};
+const getLower = () => {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
 
 // Generates uppercase
-const getUpper = () => {};
+const getUpper = () => {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
 
 // Generates numbers
-const getNumbers = () => {};
+const getNumbers = () => {
+	return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+};
 
 // Generates symbols
-const getSymbols = () => {};
+const getSymbols = () => {
+	const specialChars = [
+		'@',
+		'$',
+		'%',
+		'*',
+		'^',
+		'<',
+		'>',
+		'?',
+		'!',
+		'(',
+		')',
+		'[',
+		']',
+		'{',
+		'}',
+		"'",
+	];
+	return specialChars[Math.floor(Math.random() * specialChars.length)];
+};
+
+console.log(getSymbols());
