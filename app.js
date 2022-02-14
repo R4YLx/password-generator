e670d3e52be8fabc2ae46e630ef6987b43df5902
Raymond Lam <raymond.m.lam@gmail.com>
2022-02-14 15:47:41 +0100

    Added hover effects on btns and started on JS

---
+++ app.js
@@ -0,0 +1,15 @@
+/**
+ * Functions for generating parameters
+ */
+
+// Generates lowercase
+const getLower = () => {};
+
+// Generates uppercase
+const getUpper = () => {};
+
+// Generates numbers
+const getNumbers = () => {};
+
+// Generates symbols
+const getSymbols = () => {};
