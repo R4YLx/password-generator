6f5d3b1b760554ecfe70f4eeec8ccfe74d5a293b
Raymond Lam <raymond.m.lam@gmail.com>
2022-02-17 23:15:27 +0100

    Simplified the generate function and characters variable

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Password-Generator
 
-<!-- Try out my [Password Generator](https://rays-password-generator.netlify.app/) -->
+Try out my [Password Generator](https://rays-password-generator.netlify.app/)
 
 Make your password as strong as a Super Saiyan!
+
+**Note: I'm still trying to improve my script. Feedback are welcome!**
 
 ## Languages and tools
 
--- app.js
+++ app.js
@@ -5,92 +5,55 @@
 const passwordEl = document.querySelector('#password');
 const copyBtnEl = document.querySelector('.copyBtn');
 const pLengthEl = document.querySelector('#length');
-const pUpperEl = document.querySelector('#uppercase');
-const pLowerEl = document.querySelector('#lowercase');
-const pNumbersEl = document.querySelector('#numbers');
-const pSymbolsEl = document.querySelector('#symbols');
 const generateBtnEl = document.querySelector('#generate');
+const inputEl = document.querySelectorAll("input[type='checkbox']");
 
 /**
- * Function for generating parameters using ASCII
- * https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/ASCII-Table.svg/1261px-ASCII-Table.svg.png
+ * Characters for password
+ *
  */
 
-// Function for getting ASCII characters
-const getCharsForArray = (first, last) => {
-	const array = [];
-	for (let i = first; i <= last; i++) {
-		array.push(i);
-	}
-	return array;
+const characters = {
+	uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+	lowercase: 'abcdefghijklmnopqrstuvwxyz',
+	numbers: '0123456789',
+	symbols: '!;,.%/(){}#+-:_"@|`$=*^[]?&~',
 };
 
-// Generates lowercase
-const getLower = getCharsForArray(97, 122);
-
-// Generates uppercase
-const getUpper = getCharsForArray(65, 90);
-
-// Generates number
-const getNumber = getCharsForArray(48, 57);
-
-// Generates symbol
-const getSymbol = getCharsForArray(33, 47).concat(
-	getCharsForArray(58, 64)
-		.concat(getCharsForArray(91, 96))
-		.concat(getCharsForArray(123, 126))
-);
-
 /**
- * Generates final password
+ * Generates password
  */
 
-const generatePassword = (
-	length,
-	includeUpper,
-	includeLower,
-	includeNumber,
-	includeSymbol
-) => {
-	let charCodes = getLower;
-	let inputs = includeUpper + includeLower + includeNumber + includeSymbol;
-	if (includeUpper) charCodes = charCodes.concat(getUpper);
-	if (includeSymbol) charCodes = charCodes.concat(getSymbol);
-	if (includeNumber) charCodes = charCodes.concat(getNumber);
-	if (inputs === 0) {
-		return '';
-	}
+const generatePassword = () => {
+	let password = '';
+	let pLength = pLengthEl.value;
+	let selectedInputs = '';
 
-	const passwordCharacters = [];
-	for (let i = 0; i < length; i++) {
-		const characterCode =
-			charCodes[Math.floor(Math.random() * charCodes.length)];
-		passwordCharacters.push(String.fromCharCode(characterCode));
+	inputEl.forEach(inputCheckbox => {
+		if (inputCheckbox.checked) {
+			selectedInputs += characters[inputCheckbox.name];
+		}
+	});
+
+	if (!selectedInputs) {
+		passwordEl.innerText = 'Please select character type';
+	} else {
+		for (let i = 0; i < pLength; i++) {
+			password +=
+				selectedInputs[Math.floor(Math.random() * selectedInputs.length)];
+		}
+
+		passwordEl.innerText = password;
 	}
-	return passwordCharacters.join('');
 };
 
 /**
  * Click events
  */
 
-// Generates password
+// Generates password on click
 generateBtnEl.addEventListener('click', () => {
-	const length = Number(pLengthEl.value);
-	const includeUppers = pUpperEl.checked;
-	const includeLowers = pLowerEl.checked;
-	const includeNumbers = pNumbersEl.checked;
-	const includeSymbols = pSymbolsEl.checked;
-
-	const password = generatePassword(
-		length,
-		includeUppers,
-		includeLowers,
-		includeNumbers,
-		includeSymbols
-	);
-
-	passwordEl.innerText = password;
+	generatePassword();
 });
 
 // Copy to clipboard
